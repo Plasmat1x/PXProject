4ae2ef6b37847fa02db96a245fae60f9d161c30b
Plasmat1x <damaghack@gmail.com>
2023-01-31 13:21:33 +0700

    fix cmakelists

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off
 
 echo Configurating
-cmake -H. -B junk/CMakeLearn_release -G "Visual Studio 17 2022"
+cmake -H. -B build/CMakeLearn_release -G "Visual Studio 17 2022"
 if %ERRORLEVEL% GEQ 1 EXIT /B 1
 
 echo Build Release
